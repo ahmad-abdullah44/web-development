c9762d403768b807ec78ffaf4318eb82ad3ec5f8
ahmad-abdullah44 <ahmad.abdullah44@yahoo.com>
2023-11-07 10:58:12 +0500

    Learning JavaScript

---
+++ Web_Design_07112023/my_first.js
@@ -0,0 +1,22 @@
+//Javascript Comments and typeof
+/*console.log("hasan");
+console.log(typeof("hasan"));
+console.log(typeof("2"));
+console.log(typeof(1));
+console.log(typeof(true));
+var a=5;
+console.log(typeof(a)); 
+*/
+
+//Logical Operators
+/* var currentTime = 10;
+ console.log(currentTime > 9 && currentTime < 17); */
+
+/* var currentTime = 10;
+console.log(currentTime > 9 || currentTime < 17); */
+
+// console.log(22 % 5);; //2
+
+var PetHungry = true;
+PetHungry = !PetHungry;
+console.log(PetHungry);
